be02af2f9eceeed776eb335b7acf6fed30d09116
ccslabs <ccslabs@hotmail.com>
2015-02-23 03:00:59 +0000

    Initial Commit

---
+++ RaptorWebWalker/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace RaptorWebWalker
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblAuthorized = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblAuthorized
+            // 
+            this.lblAuthorized.AutoSize = true;
+            this.lblAuthorized.Location = new System.Drawing.Point(193, 239);
+            this.lblAuthorized.Name = "lblAuthorized";
+            this.lblAuthorized.Size = new System.Drawing.Size(79, 13);
+            this.lblAuthorized.TabIndex = 1;
+            this.lblAuthorized.Text = "Unauthorized   ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(262, 239);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(275, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Agent of The Computer Crime Service. (CCS-LABS.COM)";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::RaptorWebWalker.Properties.Resources.hand_print_blue;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(33, 40);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Maroon;
+            this.ClientSize = new System.Drawing.Size(549, 261);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblAuthorized);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "frmMain";
+            this.TransparencyKey = System.Drawing.Color.Maroon;
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblAuthorized;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
